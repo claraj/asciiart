24ce11ab26713d0a9eb415c5ee153f84fdcdcd6e
clara <clara@clara.com>
2016-01-19 19:33:55 -0600

    status

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 import setuptools
 
 setup(name='AsciiArt',
-    version='1.0.dev',
+    version='0.1.dev',
     description='Ascii "art" from images',
     author='clara',
-    classifiers=['Development Status :: 3 - Alpha',
-        Programming Language :: Python :: 3.4],
     packages=find_packages(exclude=['tests']),
     install_requires=['pillow'])
